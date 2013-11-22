88f8157637c016d6e1a5c86558b2396e53351e7a
akuraru <akuraru@gmail.com>
2013-11-22 16:56:38 +0900

    Initial Commit

---
+++ QiitaTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  QiitaTest
+//
+//  Created by akuraru on 2013/11/22.
+//  Copyright (c) 2013年 akuraru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ QiitaTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  QiitaTest
+//
+//  Created by akuraru on 2013/11/22.
+//  Copyright (c) 2013年 akuraru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
